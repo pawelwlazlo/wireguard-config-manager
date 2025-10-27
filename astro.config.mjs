ddf12330ba3b5a3530a10fa2a028b8659cacf86b
Paweł Wlazło <pawel@wlazlo.dev>
2025-10-27 20:56:01 +0100

    ggIntegrate React and Tailwind CSS into Astro project with Node adapter. Update configuration and dependencies in package.json and pnpm-lock.yaml. Add global CSS file for Tailwind styles. Enhance TypeScript configuration for JSX support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
+  server: {
+    host: "0.0.0.0",
+  },
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
